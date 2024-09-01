5ba8c00c2798eb1332d14fe92705b05b89ee8c24
Abdul ELrahman A.Bahget <abosama150@gmail.com>
2024-09-02 01:26:32 +0300

    allow use photos from images.wuzzuf-data.net in src for images in app

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.wuzzuf-data.net",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
