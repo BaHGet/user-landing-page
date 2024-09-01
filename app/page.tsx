59755df625844d7f899d89e7be4f0195fa627a6e
Abdul ELrahman A.Bahget <abosama150@gmail.com>
2024-09-01 23:08:37 +0300

    make a home page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import React from 'react'
 
-export default function Home() {
+const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>hello</h1>
-    </main>
-  );
+    <div>User landing page</div>
+  )
 }
+
+export default Home
