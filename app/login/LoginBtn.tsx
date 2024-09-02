02dcc4bbd4c863161ee7f20d6f7a67d9649582a4
Abdul ELrahman A.Bahget <abosama150@gmail.com>
2024-09-02 15:18:25 +0300

    change submition behaviour and change btn bg color and make it a SSC"

--- app/login/LoginBtn.tsx
+++ app/login/LoginBtn.tsx
@@ -1,15 +1,6 @@
-'use client';
-
-import React from 'react'
-
-type Props = {
-    handleLogin : (e: any) => void
-}
-
-const LoginBtn = ( {handleLogin} : Props) => {
-    
+const LoginBtn = () => {
     return (
-        <button className="bg-[#262626] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={(e) => handleLogin(e)}>Login</button>
+        <button className="bg-[#262626] hover:bg-gray-700 text-white font-bold py-2 px-4 rounded" type='submit'>Login</button>
     )
 }
 
